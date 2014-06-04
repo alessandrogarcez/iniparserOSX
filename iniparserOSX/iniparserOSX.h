95aa3ddc92560dbbc03e2701739c584480e79a69
Alessandro Garcez <dev@alessandrogarcez.com.br>
2014-06-04 16:59:44 -0300

    Added comments to header.

--- iniparserOSX/iniparserOSX.h
+++ iniparserOSX/iniparserOSX.h
@@ -23,12 +23,30 @@
 
 /**
  *@description	Get the number of keys in a sections of loaded ini file.
- *@param		NSString	Name of section to count the number of keys.
- *@return		int			Number of keys.
+ *@param		NSString		section		Name of sectionin file.
+ *@return		int			Number of keys in informed section.
  */
 - (int)numberOfKeysInSection:(NSString *)section;
+
+/**
+ *@description	Get the name of all sections in file.
+ *@return		NSArray			Array with nameof sections.
+ */
 - (NSArray *)getSectionsName;
+
+/**
+ *@description	Get the name of keys in a section.
+ *@param		NSString		section		Name of section in file.
+ *@return		NSArray			Return an array with all keys in a informed section.
+ */
 - (NSArray *)getKeysInSection:(NSString *)section;
+
+/**
+ *@description	Get a value for a key in a section
+ *@param		NSString		key			Name of key in file.
+ *@param		NSString		section		Name of section tin file.
+ *@return		NSString		the value for informed key and section.
+ */
 - (NSString *)getValueForKey:(NSString *)key inSection:(NSString *)section;
 
 @end
